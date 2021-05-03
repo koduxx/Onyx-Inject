61ae347561fc1a5b5d1f4277fc55e5ed96cad183
koduxx <83158549+koduxx@users.noreply.github.com>
2021-05-03 21:16:44 +0100

    Add files via upload

---
+++ Injector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace onyx.INJECT
+{
+  internal class Injector
+  {
+    public static void Inject(int processId, string path)
+    {
+      IntPtr hProcess = Injector.OpenProcess(1082, false, processId);
+      IntPtr procAddress = Injector.GetProcAddress(Injector.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+      uint num1 = (uint) ((path.Length + 1) * Marshal.SizeOf(typeof (char)));
+      IntPtr num2 = Injector.VirtualAllocEx(hProcess, IntPtr.Zero, num1, 12288U, 4U);
+      Injector.WriteProcessMemory(hProcess, num2, Encoding.Default.GetBytes(path), num1, out UIntPtr _);
+      Injector.CreateRemoteThread(hProcess, IntPtr.Zero, 0U, procAddress, num2, 0U, IntPtr.Zero);
+    }
+
+    [DllImport("kernel32.dll")]
+    public static extern int SuspendThread(IntPtr hThread);
+
+    [DllImport("kernel32.dll")]
+    public static extern int ResumeThread(IntPtr hThread);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenThread(
+      int dwDesiredAccess,
+      bool bInheritHandle,
+      int dwThreadId);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool CloseHandle(IntPtr hHandle);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(
+      int dwDesiredAccess,
+      bool bInheritHandle,
+      int dwProcessId);
+
+    [DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr VirtualAllocEx(
+      IntPtr hProcess,
+      IntPtr lpAddress,
+      uint dwSize,
+      uint flAllocationType,
+      uint flProtect);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool WriteProcessMemory(
+      IntPtr hProcess,
+      IntPtr lpBaseAddress,
+      byte[] lpBuffer,
+      uint nSize,
+      out UIntPtr lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr CreateRemoteThread(
+      IntPtr hProcess,
+      IntPtr lpThreadAttributes,
+      uint dwStackSize,
+      IntPtr lpStartAddress,
+      IntPtr lpParameter,
+      uint dwCreationFlags,
+      IntPtr lpThreadId);
+  }
+}
